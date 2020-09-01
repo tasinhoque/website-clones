3e5c27c92a59e64f60fa006f6e2a3054c253be7a
Tasin Hoque <tasinhoque@gmail.com>
2020-09-01 13:27:26 +0600

    Move CssBaseLine to index.js

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Grid, CssBaseline } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import CustomCard from './CustomCard'
 import muiTemplates from './mui-templates.jpg'
 
 const Home = () => {
   return (
     <Grid container spacing={3}>
-      <CssBaseline />
       <CustomCard
         link='/mui-templates'
         text='Material UI Templates'
